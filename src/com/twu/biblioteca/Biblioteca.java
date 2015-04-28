90b5d145aa704b6afcf951b5ad2ccaba47385590
chandnirpatel <chandnir.patel@gmail.com>
2015-04-28 15:11:38 -0500

    add startApp method that prints a message

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -1,7 +1,13 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
+
 public class Biblioteca {
     public String displayWelcomeMessage() {
         return "Welcome!";
+    }
+
+    public void startApp(PrintStream printStream) {
+        printStream.println("Welcome");
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,7 +1,12 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
+
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class BibliotecaAppTest {
 
@@ -11,5 +16,16 @@ public class BibliotecaAppTest {
         String welcomeMessage = biblioteca.displayWelcomeMessage();
 
         assertEquals(welcomeMessage, "Welcome!");
+    }
+
+    @Test
+    public void shouldDisplayWelcomeMessageWhenAppStarts() {
+        PrintStream printStream = mock(PrintStream.class);
+
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.startApp(printStream);
+
+
+        verify(printStream).println("Welcome");
     }
 }
