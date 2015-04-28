46238906b9d4e451476dec85eb5938394fa1159e
chandnirpatel <chandnir.patel@gmail.com>
2015-04-28 15:33:38 -0500

    refactored getWelcomeMessage and displayWelcomeMessage

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -3,11 +3,14 @@ package com.twu.biblioteca;
 import java.io.PrintStream;
 
 public class Biblioteca {
-    public String displayWelcomeMessage() {
-        return "Welcome!";
+
+    private String welcomeMessage = "Welcome!";
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
     }
 
-    public void startApp(PrintStream printStream) {
-        printStream.println("Welcome");
+    public void displayWelcomeMessage(PrintStream printStream) {
+        printStream.println(getWelcomeMessage());
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -9,23 +10,29 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class BibliotecaAppTest {
+    private Biblioteca biblioteca;
+    private String WELCOMEMESSAGE;
+
+    @Before
+    public void setUp(){
+        biblioteca = new Biblioteca();
+        WELCOMEMESSAGE = "Welcome!";
+    }
+
 
     @Test
     public void shouldDisplayWelcomeMessage() {
-        Biblioteca biblioteca = new Biblioteca();
-        String welcomeMessage = biblioteca.displayWelcomeMessage();
+        String welcomeMessage = biblioteca.getWelcomeMessage();
 
-        assertEquals(welcomeMessage, "Welcome!");
+        assertEquals(welcomeMessage, WELCOMEMESSAGE);
     }
 
     @Test
     public void shouldDisplayWelcomeMessageWhenAppStarts() {
         PrintStream printStream = mock(PrintStream.class);
 
-        Biblioteca biblioteca = new Biblioteca();
-        biblioteca.startApp(printStream);
+        biblioteca.displayWelcomeMessage(printStream);
 
-
-        verify(printStream).println("Welcome");
+        verify(printStream).println(WELCOMEMESSAGE);
     }
 }
